a556ed986fa11a50d013fc42ecb077791d4319e4
UsernameFodder <usernamefodder@gmail.com>
2022-02-20 17:39:33 -0600

    Format with black for readability
    Use the default black configuration

--- dungeon_eos/__init__.py
+++ dungeon_eos/__init__.py
@@ -1,2 +1,9 @@
-from dungeon_eos.DungeonAlgorithm import Properties, StaticParam, DungeonData, StatusData, ReturnData, generate_floor
+from dungeon_eos.DungeonAlgorithm import (
+    Properties,
+    StaticParam,
+    DungeonData,
+    StatusData,
+    ReturnData,
+    generate_floor,
+)
 from dungeon_eos.RandomGen import RandomGenerator
--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-__version__ = '0.0.4'
+__version__ = "0.0.4"
 
 import os
 
 from setuptools import setup, find_packages
 
 setup(
-  name = 'dungeon-eos', 
-  packages = find_packages(),
-  version = __version__,
-  description = 'A package that simulates PMD EoS dungeon generation. ',
-  author = 'SkyTemple contributors', 
-  author_email = 'skytemple@capypara.de', 
-  url = 'https://github.com/SkyTemple/dungeon_eos/',
-  keywords = ['Dungeon', 'PMD', 'Explorers', 'Generator'],
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)', 
-    'Programming Language :: Python :: 3', 
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-  ],
+    name="dungeon-eos",
+    packages=find_packages(),
+    version=__version__,
+    description="A package that simulates PMD EoS dungeon generation. ",
+    author="SkyTemple contributors",
+    author_email="skytemple@capypara.de",
+    url="https://github.com/SkyTemple/dungeon_eos/",
+    keywords=["Dungeon", "PMD", "Explorers", "Generator"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
